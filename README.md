a838de100a79249a6cfb79d91156ddc8154c0749
alexnaz <alexander.nazarenko0703@gmail.com>
2026-08-31 17:51:49 -0400

    bot.py: expanded logs with message_id and relaying function readme: version 0.1 changelog and Set-Up guide

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-#Zhuyvoda Telegram Bot
+# Zhuyvoda Telegram Bot
 A simple telegram bot for personal purposes, allowing the user to relay messages from a different person on a private channel;
 
-##Version 0 
+### Version 0.1
+The bot now relays everything the owner writes in the private channel as a post to the chosen channel. Terminal now also logs message_id needed for the next installment.
+
+### Version 0 
 A simple skeleton that can be tied to a personal channel. It reads all incoming messages and logs them with all relevant details in the inner terminal.
+
+
+
+
+## Set-Up
+
+In order to use this bot, you need to download the files and tie them with a .env file containing the following information:
+> BOT_TOKEN 
+when you create the bot in Telegram using the BotFather bot (I know, apologies for the tautology), you can receive the token to tie the actual bot to this code
+> ADMIN_ID 
+Just your account's ID so the bot knows who to listen to in particular. There is a plethora of Telegram bots that can help you find your ID.
+> CHANNEL_ID 
+Your channel's unique ID. This bot allows logging the messages so all you have to do is make this bot an admin in your channel, launch the code and, while it's running, type something. In the message info you'll see the desired info. 
+> (Optional) GROUP_ID 
+If your channel has an attached supergroup (for stuff like comment sections under your posts), you can attach it to the code as well to enable the bot to answer people's comments in your stead (you're still piloting, just so we're clear). The steps to getting the ID are identical to CHANNEL_ID. Just add the bot to the supergroup as an admin and type something!
--- bot.py
+++ bot.py
@@ -3,13 +3,14 @@ import logging
 import os
 
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 
 #importing bot api token and admin's user id 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = os.getenv("ADMIN_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
 
 #setting up logs for incoming messages
@@ -17,10 +18,20 @@ logging.basicConfig(level = logging.INFO)
 
 bot = Bot(token = BOT_TOKEN)
 
-dp = Dispatcher()
+dp = Dispatcher()   
+
+#——————————————————————————Handlers—————————————————————————————
+
+
+#handler to react to admin's messages in the private channel
+@dp.message(F.chat.type == "private", F.from_user.id == ADMIN_ID)
+async def post_to_channel(message: Message):
+    await bot.send_message(chat_id=CHANNEL_ID, text=message.text)
+    await message.reply("Повідомлення надіслано") # "message sent". This is what will be written in response to your entries. Can be changed
 
 
 
+#handler to catch simple messages and display their info
 @dp.message()
 async def terminal_logs(message: Message):
 
@@ -29,9 +40,28 @@ async def terminal_logs(message: Message):
     print(f"chat.type:    {message.chat.type}")
     print(f"chat.title:   {message.chat.title}")
     print(f"from_user:    {message.from_user}")
+    print(f"message_id:   {message.message_id}")
     print(f"text:         {message.text}")
     print("---------------------------------")
 
+#separate handler to get channel's unique id
+@dp.channel_post()
+async def terminal_logs(message: Message):
+
+    print("----- New message received -----")
+    print(f"chat.id:      {message.chat.id}")
+    print(f"chat.type:    {message.chat.type}")
+    print(f"chat.title:   {message.chat.title}")
+    print(f"from_user:    {message.from_user}")
+    print(f"message_id:   {message.message_id}")
+    print(f"text:         {message.text}")
+    print("---------------------------------")
+
+
+
+
+
+#——————————————————————————"the main" part—————————————————————————————
 
 async def main():
     await dp.start_polling(bot)
